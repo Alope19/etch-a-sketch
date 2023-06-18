ef68298fb56bf52c48d88ceff86218354a215756
Anthony <anthony.dv.lopez@gmail.com>
2023-06-17 22:48:57 -0500

    made it so slider value changes dynamically
    also made slight adjustmants to style of buttons

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-// Changes color of input=color and makes its so user can interact with it
+// Global Variables
 const colorcontainer = document.querySelector(".colorcontainer")
 const color = document.querySelector('.color')
+const slider = document.querySelector('.slider')
+const sliderValue = document.querySelector('.slidervalue')
 
+// Changes color of input=color and makes its so user can interact with it
 colorcontainer.addEventListener("click",()=>{
     color.click()
     adaptiveColorChange()
@@ -11,3 +14,8 @@ function adaptiveColorChange(){
         colorcontainer.style.backgroundColor = color.value
     })
 }
+//Changes numbers above slider
+slider.addEventListener('input',()=>{
+    sliderValue.textContent =`${slider.value} x ${slider.value}`
+})
+

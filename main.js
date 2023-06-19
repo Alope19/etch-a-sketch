18d71df2a8f80a4b8d65a747e1a337cdfaae0cb5
Anthony <anthony.dv.lopez@gmail.com>
2023-06-18 21:32:20 -0500

    Made it so slider works and adjust pixels on sketchpad
    I also renamed something in the html and added new divs also small style changes still need to implement color changing the color of divs

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 const colorcontainer = document.querySelector(".colorcontainer")
 const color = document.querySelector('.color')
 const slider = document.querySelector('.slider')
-const sliderValue = document.querySelector('.slidervalue')
+const sliderText = document.querySelector('.slidertext')
+const sketchpad = document.querySelector('.sketchpad')
+
 
 // Changes color of input=color and makes its so user can interact with it
 colorcontainer.addEventListener("click",()=>{
@@ -16,6 +18,25 @@ function adaptiveColorChange(){
 }
 //Changes numbers above slider
 slider.addEventListener('input',()=>{
-    sliderValue.textContent =`${slider.value} x ${slider.value}`
+    sliderText.textContent =`${slider.value} x ${slider.value}`
 })
 
+// Removes any divs left when user changes slider value
+slider.addEventListener('change',()=>{
+    let pixelcontainer = document.querySelectorAll('.pixelcontainer')
+    pixelcontainer.forEach( element =>{
+        sketchpad.removeChild(element)
+    })
+//Reads slider value and creates divs accordingly
+    for(i = 0; i < slider.value; i++){
+        const div = document.createElement('div')
+        div.classList.add('pixelcontainer')
+        sketchpad.appendChild(div)
+        for(j = 0; j < slider.value; j++){
+            const div2 = document.createElement('div')
+            div2.classList.add('pixel')
+            div.appendChild(div2)
+
+        }
+    }
+})
